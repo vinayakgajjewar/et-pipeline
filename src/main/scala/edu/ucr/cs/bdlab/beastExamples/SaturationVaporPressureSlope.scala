58aa7bc31db07a34868ab7a7a1ac9f308b46dc2e
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2023-05-16 20:55:43 -0700

    Convert to deg. C before calculating Delta.

--- src/main/scala/edu/ucr/cs/bdlab/beastExamples/SaturationVaporPressureSlope.scala
+++ src/main/scala/edu/ucr/cs/bdlab/beastExamples/SaturationVaporPressureSlope.scala
@@ -17,9 +17,19 @@ object SaturationVaporPressureSlope {
   /*
    * Equation 11 (in the 2014 paper)
    * T_a is air temperature (K).
+   * This computation is an approximation that doesn't use surface temperature (which isn't commonly measured).
    */
   def computeApproxSaturationVaporPressureSlope(T_a: RasterRDD[Float]): RasterRDD[Float] = {
-    return T_a.mapPixels(x => (4098.0f * (0.6108f * exp((17.27f * x) / (x + 237.3f))) / pow(x + 237.3f, 2.0f)).toFloat)
+
+    /*
+     * First we convert T_a from Kelvin to degrees Celsius.
+     */
+    val T_a_deg_C = T_a.mapPixels(x => x - 273.15)
+
+    /*
+     * Now we can compute Delta using equation 11 from Dhungel et al. 2014.
+     */
+    return T_a_deg_C.mapPixels(x => (4098.0f * (0.6108f * exp((17.27f * x) / (x + 237.3f))) / pow(x + 237.3f, 2.0f)).toFloat)
   }
 
 }
